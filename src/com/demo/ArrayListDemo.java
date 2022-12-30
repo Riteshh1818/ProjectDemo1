78eebebfcb29de2dd87cfeff049c06b9f8706da7
Riteshh1818 <tawareritesh9696@gmail.com>
2022-12-30 19:13:35 +0530

    first commit: arraylist

---
+++ src/com/demo/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Ritesh");
+		list.add("Saurabh");
+		list.add("Aakash");
+		
+		System.out.println(list);
+	}
+
+}
